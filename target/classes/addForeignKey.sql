dddbb589ddf691851a231d6c47527ad793bf2a21
Matteo Susca <matteo.susca@studio.unibo.it>
2023-04-21 10:37:18 +0200

    gitignore

--- target/classes/addForeignKey.sql
+++
@@ -1,2 +0,0 @@
---changeset matte:1.0.0-add-foreign-key
-ALTER TABLE employee ADD CONSTRAINT fk_employee_person FOREIGN KEY (id) REFERENCES person (id);
--- target/classes/insertEmployee.sql
+++
@@ -1,9 +0,0 @@
---liquibase formatted sql
---changeset matte:1.0.0-insert
-INSERT INTO person (id, name, surname, date_of_birth) VALUES (1, 'John', 'Smith', '1980-01-01');
-INSERT INTO person (id, name, surname, date_of_birth) VALUES (2, 'Mary', 'Smith', '1985-01-01');
-INSERT INTO person (id, name, surname, date_of_birth) VALUES (3, 'Peter', 'Smith', '1990-01-01');
-
-INSERT INTO employee (id, salary, department) VALUES (1, 1000, 'IT');
-INSERT INTO employee (id, salary, department) VALUES (2, 2000, 'HR');
-INSERT INTO employee (id, salary, department) VALUES (3, 3000, 'Sales');
